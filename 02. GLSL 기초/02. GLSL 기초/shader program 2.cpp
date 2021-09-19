d24335dd8ac7f544de72f061d4581e7dd1d6b5bb
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2021-09-19 11:58:10 +0900

    02. GLSL 기초 프로그램 2 추가

---
+++ 02. GLSL 기초/02. GLSL 기초/shader program 2.cpp
@@ -0,0 +1,128 @@
+#include "stdafx.h"
+
+void make_vertexShaders();
+void make_fragmentShaders();
+GLuint make_shaderProgram();
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+
+GLuint shaderID;
+GLint width, height;
+
+int main(int argc, char** argv)
+{
+	width = 500;
+	height = 500;
+
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(width, height);
+	glutCreateWindow("Example1");
+
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	//--- 세이더 읽어와서 세이더 프로그램 만들기
+	make_vertexShaders(); //--- 버텍스 세이더 만들기
+	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	shaderID = make_shaderProgram(); //--- 세이더 프로그램 만들기
+
+	glutDisplayFunc(drawScene); //--- 출력 콜백 함수
+	glutReshapeFunc(Reshape);
+
+	glutMainLoop();
+}
+
+GLuint vertexShader;
+void make_vertexShaders()
+{
+	const GLchar* vertexShaderSource;
+	std::ifstream vertexin("vertex.glsl");
+
+	//--- 버텍스 세이더 읽어 저장하고 컴파일 하기
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);			// 비주얼 스튜디오에서 컴파일되는것이 아니라 함수 내에서 컴파일
+
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader error\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+GLuint fragmentShader;
+void make_fragmentShaders()
+{
+	const GLchar* fragmentShaderSource;
+	std::ifstream fragmentin("fragment.glsl");
+
+	//--- 프래그먼트 세이더 읽어 저장하고 컴파일하기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: fragment shader error\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+GLuint make_shaderProgram()
+{
+	GLuint ShaderProgramID;
+	ShaderProgramID = glCreateProgram(); //--- 세이더 프로그램 만들기
+
+	glAttachShader(ShaderProgramID, vertexShader); //--- 세이더 프로그램에 버텍스 세이더 붙이기
+	glAttachShader(ShaderProgramID, fragmentShader); //--- 세이더 프로그램에 프래그먼트 세이더 붙이기
+
+	glLinkProgram(ShaderProgramID); //--- 세이더 프로그램 링크하기
+
+	glDeleteShader(vertexShader); //--- 세이더 객체를 세이더 프로그램에 링크했음으로, 세이더 객체 자체는 삭제 가능
+	glDeleteShader(fragmentShader);
+
+	GLint result;
+	GLchar errorLog[512];
+	glGetProgramiv(ShaderProgramID, GL_LINK_STATUS, &result); // ---세이더가 잘 연결되었는지 체크하기
+	if (!result) {
+		glGetProgramInfoLog(ShaderProgramID, 512, NULL, errorLog);
+		std::cerr << "ERROR: shader program 연결 실패\n" << errorLog << std::endl;
+		return false;
+	}
+	glUseProgram(ShaderProgramID); //--- 만들어진 세이더 프로그램 사용하기
+	//--- 여러 개의 세이더프로그램 만들 수 있고, 그 중 한개의 프로그램을 사용하려면
+	//--- glUseProgram 함수를 호출하여 사용 할 특정 프로그램을 지정한다.
+	//--- 사용하기 직전에 호출할 수 있다.
+	return ShaderProgramID;
+}
+
+GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
+{
+	GLfloat rColor, gColor, bColor;
+
+	rColor = gColor = bColor = 1.0;
+	glClearColor(rColor, gColor, bColor, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(shaderID);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3); //--- 렌더링하기
+
+	glutSwapBuffers(); // 화면에 출력하기
+}
+
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
+{
+	glViewport(0, 0, w, h);
+}
---
+++ 02. GLSL 기초/02. GLSL 기초/stdafx.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#include <gl/glew.h>								//--- 필요한 헤더파일 include
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
