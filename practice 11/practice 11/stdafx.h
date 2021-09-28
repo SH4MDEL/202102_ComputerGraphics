adc6de73072eccb9d52e865cd1b0b06569c8e0bd
Seungjun Lee <45813451+SH4MDEL@users.noreply.github.com>
2021-09-28 13:10:07 +0900

    03주차 실습 11 완성

---
+++ practice 11/practice 11/stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <random>
+#include <math.h>
+
+#include <gl/glew.h>								//--- 필요한 헤더파일 include
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
